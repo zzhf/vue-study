562d9e7fb3b58e0f705f357d025cb2f7aa318945
zzhf <zzzhf00@sina.com>
2017-10-29 23:57:37 +0800

    新增alert组件

---
+++ src/base/components/Alert.vue
@@ -0,0 +1,55 @@
+<template>
+    <transition name="dc-alert-fade">
+        <div class="dc-alert" :class="[ typeClass ]" v-show="visible">
+            <i class="dc-alert-icon" v-if="showIcon"></i>
+            <div class="dc-alert-content">
+                <span class="dc-alert-title" v-if="title">{{ title }}</span>
+                <slot>
+                    <p class="dc-alert-description" v-if="description">{{ description }}</p>
+                </slot>
+                <i class="dc-alert-closebtn" v-show="closable" @click="click"></i>
+            </div>
+        </div>
+    </transition>
+</template>
+
+<script>
+    export default {
+        name: 'DcAlert',
+
+        props: {
+            title: {
+                type: String,
+                default: ''
+            },
+            description: {
+                type: String,
+                default: ''
+            },
+            visible: {
+                type: Boolean,
+                default: false
+            },
+            showIcon: {
+                type: Boolean,
+                default: true
+            },
+            closable: {
+                type: Boolean,
+                default: true
+            }
+        },
+
+        data() {
+            return {
+            }
+        },
+
+        methods: {
+            close() {
+                this.visible = false;
+                this.$emit('close');
+            }
+        }
+    }
+</script>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,19 +1,25 @@
 <template>
-  <dc-button>{{ msg }}</dc-button>
+    <dc-button>{{ msg }}</dc-button>
+    <dc-alert>
+        我是一个默认的弹出框
+    </dc-alert>
 </template>
 
 <script>
 import DcButton from './../base/components/BaseButton';
+import DcAlert from './../base/components/Alert';
 
 export default {
   name: 'HelloWorld',
+
   components: {
-    DcButton
+        DcButton, DcAlert
   },
+
   data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
-    }
+        return {
+          msg: 'Welcome to Your Vue.js App'
+        }
   }
 }
 </script>
